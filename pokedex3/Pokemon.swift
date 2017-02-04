bac018252e1f989e0432aab234ed0af596d5b3d7
jasonhilimire <jasonhilimire@mac.com>
2017-02-03 23:17:25 -0700

    data protection

--- pokedex3/Pokemon.swift
+++ pokedex3/Pokemon.swift
@@ -22,6 +22,74 @@ class Pokemon {
     private var _attack: String!
     private var _nextEvolutionTxt: String!
     private var _pokemonURL: String!
+   
+    
+    //DATA PROTECTION ensure that we are getting and actual value and if we dont instead of nil just returns an empty string
+    
+    var description: String {
+        
+        if _description == nil {
+            
+            _description = ""
+        }
+        return _description 
+    }
+    
+    var type: String {
+        
+        if _type == nil {
+            
+            _type = ""
+            
+        }
+        return _type
+    }
+    
+    var defense: String {
+        
+        if _defense == nil {
+            
+            _defense = ""
+        }
+        return _defense
+    }
+    
+    var height: String {
+        
+        if _height == nil {
+        
+        _height = ""
+        }
+        return _height
+    }
+    
+    var weight: String {
+        
+        if _weight == nil {
+            
+            _weight = ""
+        }
+        return _weight
+    }
+    
+    var attack: String {
+        
+        if _attack == nil {
+            
+            _attack = ""
+        }
+        return _attack
+    }
+    
+    
+    var nextEvolutionText: String {
+        
+        if _nextEvolutionTxt == nil {
+            
+            _nextEvolutionTxt = ""
+        }
+        return _nextEvolutionTxt
+    }
     
     var name: String {
         return _name
